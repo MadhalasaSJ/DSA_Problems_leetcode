5ba579db5c791a709af8a077188e69e7ca8d2c8a
MadhalasaSJ <112635902+MadhalasaSJ@users.noreply.github.com>
2025-09-09 18:57:36 +0530

    Time: 12 ms (88.62%) | Memory: 18.1 MB (12.19%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -4,16 +4,16 @@ class Solution:
         start = 0
         max_length = 0
         
-        # Traverse the string and update the count of characters
+        
         for end in range(len(s)):
-            # If the character is already in the substring, update start
-            if s[end] in char_count and char_count[s[end]] >= start:
-                start = char_count[s[end]] + 1  # Move start to the right of the duplicate
             
-            # Update the last seen index of the character
+            if s[end] in char_count and char_count[s[end]] >= start:
+                start = char_count[s[end]] + 1  
+            
+            
             char_count[s[end]] = end
             
-            # Calculate the length of the current substring
+            
             max_length = max(max_length, end - start + 1)
         
         return max_length           
